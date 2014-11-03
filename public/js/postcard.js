1f989da9f248dc28ce8a6e8eced49e028181d29c
leikou <leikou@163.com>
2014-11-03 17:12:55 +0800

    Add contact manager in postcard.js

--- public/js/postcard.js
+++ public/js/postcard.js
@@ -1,16 +1,48 @@
 (function($) {
+    var domain = "http://" + window.location.host;
+    var userOpenId = "";
+    
+
+    /*************** Util begin   ***************************/
+    $.extend(HC, {
+        log: function(data) {   //日志记录
+            console.log(data);
+        },
+        showError(message, code) {
+            if (code) {
+                message += " code: " + code;
+            }
+            alert(message);
+        },
+        goToPage: function(url) {
+            window.location.href = url;        
+        },
+        getNonceStr: function() {
+            return "" + new Date().getTime();
+        },
+    });
+
+    /*************** Util begin   ***************************/
+
+    /*************** Postcard define begin ******************/
     function PostcardImage() {
         var varCollection = {
             url: "",
-            template: "",
+            templateIndex: 1,
             offsetX: 0,
             offsetY: 0,
             isRotate: false          // 图片是否旋转(模板不同旋转不同)
         };
-
         $.extend(PostcardImage.prototype, {
+            isComplete: function() {
+                if ( ! varCollection.url) {
+                    return false;
+                }
+                return true;
+            },
             setVars: function(paramData) {
                 $.extend(varCollection, paramData);
+                return this;
             },
             getUrl: function() {
                 return varCollection.url;
@@ -19,11 +51,11 @@
                 varCollection.url = url;
                 return this;
             },
-            getTemplate: function() {
-                return varCollection.template;
+            getTemplateIndex: function() {
+                return varCollection.templateIndex;
             },
-            setTemplate: function(template) {
-                varCollection.template = template;
+            setTemplate: function(templateIndex) {
+                varCollection.templateIndex = templateIndex;
                 return this;
             },
             getOffsetX: function() {
@@ -60,8 +92,15 @@
         };
 
         $.extend(Address.prototype, {
+            isComplete: function() {
+                if ( ! varCollection.name || ! varCollection.address || ! varCollection.zipcode) {
+                    return false;
+                }
+                return true;
+            },
             setVars: function(paramData) {
                 $.extend(varCollection, paramData);
+                return this;
             },
             getName: function() {
                 return varCollection.name;
@@ -89,20 +128,27 @@
 
     function Message() {
         var varCollection = {
-            receiptNickname: "",            // 收件人昵称
+            salutation: "",            // 收件人昵称
             content: "",
-            senderNickname: ""              // 发件人昵称
+            signature: ""              // 发件人昵称
         };
 
         $.extend(Message.prototype, {
+            isComplete: function() {
+                if ( ! varCollection.salutation || ! varCollection.content || ! varCollection.signature) {
+                    return false;
+                }
+                return true;
+            },
             setVars: function(paramData) {
                 $.extend(varCollection, paramData);
+                return this;
             },
-            getReceiptNickname: function() {
-                return varCollection.receiptNickname;
+            getSalutation: function() {
+                return varCollection.salutation;
             },
-            setTitle: function(receiptNickname) {
-                varCollection.receiptNickname = receiptNickname;
+            setSalutation: function(salutation) {
+                varCollection.salutation = salutation;
                 return this;
             },
             getContent: function() {
@@ -112,11 +158,11 @@
                 varCollection.content = content;
                 return this;
             },
-            getSenderNickname: function() {
-                return varCollection.senderNickname;
+            getSignature: function() {
+                return varCollection.signature;
             },
-            setSenderNickname: function(senderNickname) {
-                varCollection.senderNickname = senderNickname;
+            setSignature: function(signature) {
+                varCollection.signature = signature;
                 return this;
             }
         });
@@ -126,14 +172,31 @@
         var varCollection = {
             image: new PostcardImage(),        // 明信片图片
             receiptAddress: new Address(),     // 收件人地址
-            senderAddress: new Address(),      // 发件人地址
-            message: new Message,              // 明信片祝福信息
+            message: new Message(),            // 明信片祝福信息
             postmark: ""                       // 邮戳
         };
 
         $.extend(Postcard.prototype, {
+            isComplete: function() {
+                if ( 
+                    ! varCollection.image.isComplete() ||
+                    ! varCollection.receiptAddress.isComplete() ||
+                    ! varCollection.message.isComplete()
+                ) {
+                    return false;
+                }
+                return true;
+            },
+            getUnCompleteInfo: function() {
+                return {
+                    image: varCollection.image.isComplete(),
+                    receiptAddress: varCollection.receiptAddress.isComplete(),
+                    message: varCollection.message.isComplete(),
+                };
+            },
             setVars: function(paramData) {
                 $.extend(varCollection, paramData);
+                return this;
             },
             getImage: function() {
                 return varCollection.image;
@@ -150,6 +213,200 @@
         });
     }
 
+    /*************** Postcard define end ******************/
 
-    window.Postcard = Postcard;
+    /*************** Contacts begin ***********************/
+
+    function Contacts() {
+        var addressBook = [];
+        var chosedIndex = 0;                // 被选中的地址序号
+        var isInited = false;
+
+        $.extend(Contacts.prototype, {
+            addContact: function(address) { // address为Address对象
+                var url = domain . "/postcard/addcontact";
+                var params = {
+                    userName: userOpenId,
+                    contactName: address.getName(),
+                    address: address.getAddress(),
+                    zipCode: address.getZipcode(),
+                };
+                $.ajax({
+                    url: url,
+                    type: "POST",
+                    data: params,
+                    dataType: "json",
+                    timeout: 10000,
+                }).done(function(data) {
+                    addressBook.push(address);
+                }).fail(function(xmlhttprequest, err, e) {
+                    if (err == "timeout") {
+                        HC.showError("网速不给力，请稍候再试哦");
+                    } else {
+                        HC.showError("add contact failed !");
+                    }
+                });
+
+                return this;
+            },
+            setChosenIndex: function(index) {
+                // 超出数组大小，置为0
+                index = index > addressBook.length ? 0 : index;
+                chosenIndex = index;
+                return this;
+            },
+            getChoseIndex: function() {
+                return addressBook[chosedIndex];
+            },
+            fetchContacts: function() {
+                addressBook = [];
+                self = this;
+                var url = domain + "/postcard/contacts?userName" + this.userName;
+                $.ajax({
+                    url: url,
+                    type: "GET",
+                    dataType: "json",
+                    timeout: 10000,
+                }).done(function(data) {
+                    $.each(data, function(i, v) {
+                        var address = new Address();
+                        address.setName(v.contactName)
+                            .setAddress(v.address)
+                            .setZipcode(v.zipCode);
+                        addressBook.push(address);
+                    });
+                    self.showContacts();
+                }).fail(function(xmlhttprequest, err, e) {
+                    if (err == "timeout") {
+                        HC.showError("网速不给力，请稍候再试哦");
+                    } else {
+                        HC.showError("add contact failed!");
+                    }
+                });
+
+                return this;
+            },
+            showContacts: function() {
+                if (isInited == false) {
+                    this.fetchContacts();
+                    isInited = true;
+                }
+
+                // TODO render contact
+            },
+        });
+    }
+
+    /*************** Contacts end *************************/
+
+    /*************** Order begin *****************/
+    function Order() {
+        this.orderId = null;
+        this.postcard = new Postcard();
+        this.userName = userOpenId;                     // 用户的OpenId
+
+        var orderIsInited = false;
+    }
+    $.extend(Order.prototype, {
+        placeOrder: function() {
+            var url = domain + "/postcard/placeorder?nonce=" + HC.getNonceStr();
+            var params = {
+                templateIndex: this.postcard.getImage().getTemplateIndex(),
+                offsetX: this.postcard.getImage().getOffsetX(),
+                offsetY: this.postcard.getImage().getOffsetY(),
+                userName: this.userName,
+                userPicUrl: this.postcard.getImage().getUrl(),
+            };
+            $.post(
+                url, 
+                params, 
+                function success(data) {
+                    if (data.code != "0") {
+                        HC.showError("Place order failed!", data.code);
+                    } else {
+                        var url = domain + "postcard/editmessage" 
+                            + data.orderId + "?nonce=" + HC.getNonceStr();
+                        HC.goToPage(url);                        
+                    }
+                }
+                "json"
+            );
+        },
+        updateOrder: function(params, successCallback) {
+            var url = domain + "postcard/updateOrder/" + this.orderId
+                + "?nonce=" + HC.getNonceStr();
+            $.ajax({
+                url: url,
+                type: "POST",
+                data: params,
+                dataType: "json",
+                timeout: 10000,
+            }).done(function(data) {
+                successCallback();
+            }).fail(function(xmlhttprequest, err, e) {
+                if (err == "timeout") {
+                    HC.showError("网速不给力，请稍候再试");
+                } else {
+                    HC.showError("update order failed!");
+                }
+            });
+        },
+        updateImageForOrder: function() {
+            var params = {
+                templateIndex: this.postcard.getImage().getTemplateIndex(),
+                offsetX: this.postcard.getImage().getOffsetX(),
+                offsetY: this.postcard.getImage().getOffsetY(),
+            };
+            return this.updateOrder(params, successCallback);
+        }
+        submitMessage: function() {
+            var params = {
+                salutation: this.postcard.getMessage().getSalutation(),
+                message: this.postcard.getMessage().getContent(),
+                signature: this.postcard.getMessage().getSignature(),
+            };
+            return this.updateOrder(params, successCallback);
+        },
+        requestVoice: function() {
+            var url = domain + "/postcard/requestvoice/" + this.orderId
+                + "?nonce=" + HC.getNonceStr();
+            $.get(
+                url,
+                function success(data) {
+                    if (data.errcode != "0" && data.code != "0") {
+                        HC.showError("Send voice request failed (" + data.errmsg + ")", data.errcode);
+                    } else {
+                        if (typeof WeixinJSBridge == "undefined") {
+                            HC.showError("不支持方法'closeWindow'. 请在微信浏览器中运行");
+                        } else {
+                            WeixinJSBridge.call("closeWindow");
+                        }
+                    }
+                },
+                "json"
+            );
+        },
+        submitAddress: function() {
+            var params = {
+                name: this.postcard.getAddress().getName(),
+                address: this.postcard.getAddress().getAddress(),
+                zipcode: this.postcard.getZipcode().getZipcode(),
+            };
+            return this.updateOrder(params, successCallback);
+        },
+        goToPay: function() {
+            var url = domain + "/wxpay/asyncmakepicture/" + this.orderId;
+            $.post(
+                url,
+                function success(data) {
+                    // TODO callpay
+                }
+            );
+        },
+    });
+    /*************** Order end *******************/
+
+
+    window.order = new Order();
+    window.contacts = new Contacts();
 })(jQuery);
