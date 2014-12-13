9074bc78397817e325864af51825e57fa1eb208f
491055741 <491055741@qq.com>
2014-12-13 18:42:19 +0800

    complete page

--- public/js/complete.js
+++ public/js/complete.js
@@ -2,12 +2,12 @@
 var orderId = '0';
 
 $(document).on("pageinit", "#completePage", function() {
-    output("completePage init");
+//    output("completePage init");
     orderId = $('#orderId').val();
 });
 
 $(document).on("pageshow", "#completePage", function() {
-    output("completePage show");
+//    output("completePage show");
     var postcardurl = "http://" + window.location.host + "/postcard/shareimage/" + orderId;
     bShare.addEntry({
         title: "我的趣邮明信片",
