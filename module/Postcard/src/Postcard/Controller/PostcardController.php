76098e875cd826034a5d86b2a6c5b6fd98adb8cb
491055741 <491055741@qq.com>
2015-01-14 17:46:58 +0800

    fix address css

--- module/Postcard/src/Postcard/Controller/PostcardController.php
+++ module/Postcard/src/Postcard/Controller/PostcardController.php
@@ -30,7 +30,7 @@ define('LEFT', 0);
 define('RIGHT', 1);
 define('CENTER', 2);
 
-define('JS_TAG', '201501141634');
+define('JS_TAG', '20150114164');
 
 class PostcardController extends AbstractActionController
 {
