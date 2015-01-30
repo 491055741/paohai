79a385138db407753667795f77f6f320a9b08546
leikou <leikou@163.com>
2015-01-30 18:16:55 +0800

    Modify order_table.picUrl from varchar(32) to varchar(1000)

--- config/products_sql/activity.sql
+++ config/products_sql/activity.sql
@@ -71,3 +71,4 @@ CREATE TABLE `activity_join_record` (
 
 
 ALTER TABLE `order_table` ADD `activityId` INT(11) UNSIGNED NOT NULL DEFAULT '1' COMMENT 'referer to field: id of table activity. 1 for default activity';
+ALTER TABLE `order_table` MODIFY `picUrl` varchar(1000) DEFAULT NULL;
