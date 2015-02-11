e5c6dd7aa24aa7ffc3c187fe95a2710ba4abf61d
491055741 <491055741@qq.com>
2015-02-11 20:03:16 +0800

    complete page

--- public/js/complete.js
+++ public/js/complete.js
@@ -1,12 +1,6 @@
 (function($) {
     orderId = $('#orderId').val();
     $(function() {
-        var postcardurl = "http://" + window.location.host + "/postcard/shareimage/" + orderId;
-        bShare.addEntry({
-            title: "我的趣邮明信片",
-            url: postcardurl,
-            summary: "",
-            pic: postcardurl
-        });
+//        var postcardurl = "http://" + window.location.host + "/postcard/shareimage/" + orderId;
     })
 })(jQuery);
