284a5bc0b40696c6b131e58067ec4d33a1516229
491055741 <491055741@qq.com>
2015-02-13 12:07:31 +0800

    ...

--- module/Postcard/src/Postcard/Controller/PostcardController.php
+++ module/Postcard/src/Postcard/Controller/PostcardController.php
@@ -210,7 +210,21 @@ class PostcardController extends AbstractActionController
             $selectedTemplateIndex = array_keys($imgTemplates)[0];
         }
 
-        $userName = $this->getRequest()->getQuery('username') ?: DEFAULT_USER;
+        $userName = $this->getRequest()->getQuery('username');
+        if ( ! $userName) {
+            if ($picUrl == DEFAULT_PICURL) {
+                $userName = DEFAULT_USER;
+            } else {
+                $currUrl = $this->getCurrentUrl();
+                $oauthUrl = $this->getServiceLocator()
+                    ->get("Wechat\Service\OauthService")
+                    ->setRequest($this->getRequest())
+                    ->getOauthUrl($currUrl);
+                Header("Location: $oauthUrl");
+                exit;
+            }
+        }
+
         $viewModel =  new ViewModel(array(
             'templateIndex' => $selectedTemplateIndex,
             'offsetX' => $offsetX,
@@ -226,6 +240,22 @@ class PostcardController extends AbstractActionController
         ));
         $viewModel->setTerminal(true); // disable layout template
         return $viewModel;
+    }
+
+    private function getCurrentUrl() {
+        $uri = $this->getRequest()->getUri();
+        $scheme = $uri->getScheme();
+        $host = $uri->getHost();
+        $port = $uri->getPort();
+        $path = $uri->getPath();
+        $query = $uri->getQuery();
+        $query = trim(str_replace(ltrim($path, "/"), '', $query), "&");
+        $currUrl = $scheme . "://" . $host . ":" . $port . $path;
+        if ($query) {
+            $currUrl .= "?" . $query;
+        }
+
+        return $currUrl;
     }
 
     public function editPostcardAction()
