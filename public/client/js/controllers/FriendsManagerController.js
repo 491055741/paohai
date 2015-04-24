79895de86794afb67453b952c74237fe53cbcc55
文红军 <wenhongjun@coolhey.cn>
2015-04-24 15:59:53 +0800

    分享给朋友。

--- public/client/js/controllers/FriendsManagerController.js
+++ public/client/js/controllers/FriendsManagerController.js
@@ -94,10 +94,37 @@ postcardControllers.controller("FriendsManagerController", ["$rootScope", "$scop
             });
         };
 
-        Util.overlay.init("<h2>dkfla;sksadf</h2>");
+        Util.overlay.init("<img style='width: 100%' src='images/share-to-friends.png'/>");
 
         $scope.showOverlay = function () {
             Util.overlay.show();
         };
+
+        //wx.onMenuShareTimeline({
+        //    title: '', // 分享标题
+        //    link: '', // 分享链接
+        //    imgUrl: '', // 分享图标
+        //    success: function () {
+        //        // 用户确认分享后执行的回调函数
+        //    },
+        //    cancel: function () {
+        //        // 用户取消分享后执行的回调函数
+        //    }
+        //});
+        //
+        //wx.onMenuShareAppMessage({
+        //    title: '', // 分享标题
+        //    desc: '', // 分享描述
+        //    link: '', // 分享链接
+        //    imgUrl: '', // 分享图标
+        //    type: '', // 分享类型,music、video或link，不填默认为link
+        //    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+        //    success: function () {
+        //        // 用户确认分享后执行的回调函数
+        //    },
+        //    cancel: function () {
+        //        // 用户取消分享后执行的回调函数
+        //    }
+        //});
     }
 ]);
