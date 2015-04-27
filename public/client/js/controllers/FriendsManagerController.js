2f8a44f516ebe206bc4d0bfde621a0b49d4ff457
文红军 <wenhongjun@coolhey.cn>
2015-04-27 16:02:44 +0800

    测试分享

--- public/client/js/controllers/FriendsManagerController.js
+++ public/client/js/controllers/FriendsManagerController.js
@@ -103,31 +103,31 @@ postcardControllers.controller("FriendsManagerController", ["$rootScope", "$scop
             Util.overlay.show();
         };
 
-        //wx.onMenuShareTimeline({
-        //    title: '', // 分享标题
-        //    link: '', // 分享链接
-        //    imgUrl: '', // 分享图标
-        //    success: function () {
-        //        // 用户确认分享后执行的回调函数
-        //    },
-        //    cancel: function () {
-        //        // 用户取消分享后执行的回调函数
-        //    }
-        //});
-        //
-        //wx.onMenuShareAppMessage({
-        //    title: '', // 分享标题
-        //    desc: '', // 分享描述
-        //    link: '', // 分享链接
-        //    imgUrl: '', // 分享图标
-        //    type: '', // 分享类型,music、video或link，不填默认为link
-        //    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-        //    success: function () {
-        //        // 用户确认分享后执行的回调函数
-        //    },
-        //    cancel: function () {
-        //        // 用户取消分享后执行的回调函数
-        //    }
-        //});
+        wx.onMenuShareTimeline({
+            title: '', // 分享标题
+            link: '', // 分享链接
+            imgUrl: '', // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+
+        wx.onMenuShareAppMessage({
+            title: 'dfasf', // 分享标题
+            desc: 'dfasdf', // 分享描述
+            link: 'http://www.baiduc.com', // 分享链接
+            imgUrl: 'http://quyou.quyoucard.com/images/small/logo.jpg', // 分享图标
+            type: '', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+                // 用户确认分享后执行的回调函数
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
     }
 ]);
