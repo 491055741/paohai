d4649744208abc9eeebc16cd5929323c9f960474
文红军 <wenhongjun@coolhey.cn>
2015-05-11 20:30:28 +0800

    调试支付。

--- public/client/js/controllers/OrderController.js
+++ public/client/js/controllers/OrderController.js
@@ -82,16 +82,29 @@ postcardControllers.controller("OrderController", ["$rootScope", "$scope", "$win
                     "paySign": payParameters.paySign
                 };
 
-                WeixinJSBridge.invoke("getBrandWCPayRequest",
-                    params,
-                    function(res){
+
+                wx.chooseWXPay({
+                    timestamp: params.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                    nonceStr: params.nonceStr, // 支付签名随机串，不长于 32 位
+                    package: params.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+                    signType: params.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                    paySign: params.paySign, // 支付签名
+                    success: function (res) {
                         alert(JSON.stringify(res));
-                    if (res.err_msg == 'get_brand_wcpay_request:ok') { // pay success
-                        $location.path("/done");
-                    } else if (res.err_msg != 'get_brand_wcpay_request:cancel') { // fail with other reason, exclude user cancel
-                        alert(res.err_msg);
+                        // 支付成功后的回调函数
                     }
                 });
+
+                //WeixinJSBridge.invoke("getBrandWCPayRequest",
+                //    params,
+                //    function(res){
+                //        alert(JSON.stringify(res));
+                //    if (res.err_msg == 'get_brand_wcpay_request:ok') { // pay success
+                //        $location.path("/done");
+                //    } else if (res.err_msg != 'get_brand_wcpay_request:cancel') { // fail with other reason, exclude user cancel
+                //        alert(res.err_msg);
+                //    }
+                //});
             }
         };
 
